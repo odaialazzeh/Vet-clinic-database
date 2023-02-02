a119dda94a142b30116146f8adbc44b2a88a8ad9
odaialazzeh <odai.alazzeh@gmail.com>
2023-02-02 18:12:55 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -28,3 +28,28 @@ ALTER TABLE animals ADD COLUMN species_id INT;
 ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
 ALTER TABLE animals ADD COLUMN owner_id INT;
 ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+-- Create a table named vets with the following columns:
+CREATE TABLE vets ( 
+   id INT GENERATED ALWAYS AS IDENTITY,
+   name VARCHAR(255), 
+   age INT, 
+   date_of_graduation date,
+   PRIMARY KEY(id) 
+ );
+
+ -- Create the specializations table:
+ CREATE TABLE specializations (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    vet_id INT REFERENCES vets(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    species_id INT REFERENCES species(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+-- Create the visits table:
+CREATE TABLE visits (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    animal_id INT REFERENCES animals(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    vet_id INT REFERENCES vets(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    visit_date DATE NOT NULL
+);
+
